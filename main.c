0c09c95abf50e588bbda99492d2a60a71f79063f
ChrisDav05 <davioteotonio05@gmail.com>
2025-03-25 20:58:19 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    int arr[TAMANHO];
+    preencherAleatorio(arr, TAMANHO);
+
+    printf("Testando algoritmos de ordenação com %d elementos:\n", TAMANHO);
+    
+    testarOrdenacao(bubbleSort, arr, TAMANHO, "Bubble Sort");
+    testarOrdenacao(selectionSort, arr, TAMANHO, "Selection Sort");
+    testarOrdenacao(insertionSort, arr, TAMANHO, "Insertion Sort");
+    testarMergeSort(arr, TAMANHO);
+    testarQuickSort(arr, TAMANHO);
+    testarOrdenacao(countingSort, arr, TAMANHO, "Counting Sort");
+
+    return 0;
+}
